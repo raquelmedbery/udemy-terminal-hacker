ffc571d390e49add686963735fd3d40ac2e269e3
raquelmedbery <50762368+raquelmedbery@users.noreply.github.com>
2019-11-19 19:10:16 -0600

    added Hacker script to terminal asset

---
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Terminal.WriteLine("What's up?");
+        Terminal.WriteLine("Feel like committing a crime today?");
+        Terminal.WriteLine("That's cool, no judgement here.");
+        Terminal.WriteLine("What do you want to hack into?");
+        //Terminal.WriteLine("Be aware that the difficulty increases the higher you go!");
+        Terminal.WriteLine("\n");
+        Terminal.WriteLine("Press 1 for your friend's laptop");
+        Terminal.WriteLine("2 for your school records");
+        Terminal.WriteLine("3 for your FBI agent");
+        Terminal.WriteLine("Enter your decision");
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
