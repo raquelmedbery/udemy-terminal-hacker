5bd821783dc582442c86bf9cb8723cab09611103
raquelmedbery <50762368+raquelmedbery@users.noreply.github.com>
2019-11-21 22:29:35 -0600

    Update Hacker.cs

--- 2_Terminal_Hacker/Assets/Hacker.cs
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -7,7 +7,13 @@ public class Hacker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Terminal.WriteLine("What's up?");
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What's up user!");
         Terminal.WriteLine("Feel like committing a crime today?");
         Terminal.WriteLine("That's cool, no judgement here.");
         Terminal.WriteLine("What do you want to hack into?");
@@ -17,12 +23,40 @@ public class Hacker : MonoBehaviour
         Terminal.WriteLine("2 for your school records");
         Terminal.WriteLine("3 for your FBI agent");
         Terminal.WriteLine("Enter your decision");
-         
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnUserInput(string input)
     {
-        
+        if(input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if(input == "1")
+        {
+            Terminal.WriteLine("You chose friend's laptop!");
+        }
+        else if (input == "2")
+        {
+            Terminal.WriteLine("You chose school records");
+        }
+        else if (input == "3")
+        {
+            Terminal.WriteLine("You chose FBI agent!");
+        }
+        else if (input == "69")
+        {
+            Terminal.WriteLine("Not enough money for chicken nuggets");
+            Terminal.WriteLine("Choose again");
+        }
+        else if (input == "Kingdom Hearts")
+        {
+            Terminal.WriteLine("Fun game. Not the one we're playing" + "\n" + "though");
+            Terminal.WriteLine("Choose again");
+        }
+        else
+        {
+            Terminal.WriteLine("That is not a valid option");
+            Terminal.WriteLine("Choose again");
+        }
     }
 }
